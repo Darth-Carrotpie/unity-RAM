0527b81b135e8f6f195a8132ed78658658a7d77d
Danielius <carrotpie@gmail.com>
2018-03-19 11:37:05 +0200

    new empty script setup. UI setup for them

---
+++ Assets/MixerGroupButtons.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class MixerGroupButtons : MonoBehaviour {
+    List<AudioMixerGroup> snapshots = new List<AudioMixerGroup>();
+    public AudioMixer mixer;
+    public GameObject buttonPrefab;
+    IEnumerator fadeAfterIntermedate;
+
+
+    void Start () {
+        snapshots.AddRange((AudioMixerGroup[])mixer.GetType().GetProperty("snapshots").GetValue(mixer, null));
+
+    }
+
+
+    void Update () {
+		
+	}
+}
